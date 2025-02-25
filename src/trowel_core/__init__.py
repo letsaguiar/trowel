e52f165892f067ee3b21de9b041267eaa6afec72
Letícia Aguiar <letsaguiar@yandex.com>
2025-02-24 21:09:43 -0300

    feat: create template service

--- src/trowel_core/__init__.py
+++ src/trowel_core/__init__.py
@@ -1,6 +1,14 @@
 import click
+import jinja2
 import json
+import os
+import jinja2.tests
 import pydantic
+
+TROWEL_PATH = os.getenv('TROWEL_PATH')
+TROWEL_SRC = f"{TROWEL_PATH}/src/trowel_core"
+TROWEL_RESOURCES = f"{TROWEL_SRC}/resources"
+TROWEL_TEMPLATES = f"{TROWEL_RESOURCES}/templates"
 
 class ConfigModel(pydantic.BaseModel):
     name: str
@@ -26,6 +34,27 @@ class ConfigService:
         data = self._parser.parse(path)
         return ConfigModel(**data)
         
+class TemplateBuilder:
+    @staticmethod
+    def build(config: ConfigModel):
+        raise Exception("Method not implemented.")
+        
+class TemplateBuilderMake:
+    @staticmethod
+    def build(config: ConfigModel):
+        with open(f"{TROWEL_TEMPLATES}/Makefile") as file:
+            template = jinja2.Template(file.read())
+            output = template.render(config)
+
+        return output
+        
+class TemplateService:
+    def __init__(self, builder: TemplateBuilder):
+        self._builder = builder
+
+    def build(self, config: ConfigModel):
+        return self._builder.build(config)
+
 @click.group()
 def cli():
     """Trowel is a C builder and project management tool designed for simplicity and automation"""
@@ -35,6 +64,9 @@ def cli():
 @click.option("--path", default="./trowel.json", help="Path to your trowel config file")
 def build(path: str):
     config_service = ConfigService(ConfigParserJson)
+    template_service = TemplateService(TemplateBuilderMake)
 
     config = config_service.getConfig(path)
-    print(config)
+    template = template_service.build(config)
+
+    print(template)
