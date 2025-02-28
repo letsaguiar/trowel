695a4b5416f86503685362b02762b77111e3c2ef
Letícia Aguiar <letsaguiar@yandex.com>
2025-02-28 20:26:23 -0300

    feat: move resources to src

--- src/trowel_core/__init__.py
+++ src/trowel_core/__init__.py
@@ -8,7 +8,7 @@ import subprocess
 import tempfile
 
 TROWEL_PATH = os.getenv('TROWEL_PATH')
-TROWEL_SRC = f"{TROWEL_PATH}/src/trowel_core"
+TROWEL_SRC = f"{TROWEL_PATH}/src"
 TROWEL_RESOURCES = f"{TROWEL_SRC}/resources"
 TROWEL_TEMPLATES = f"{TROWEL_RESOURCES}/templates"
 
