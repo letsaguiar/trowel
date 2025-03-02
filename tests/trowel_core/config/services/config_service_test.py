dba7d5b59facee93555b415164676368d5dc0fb6
Letícia Aguiar <letsaguiar@yandex.com>
2025-03-02 13:34:25 -0300

    test: config service

---
+++ tests/trowel_core/config/services/config_service_test.py
@@ -0,0 +1,22 @@
+import json
+import pytest
+import pathlib
+
+from src.trowel_core.config.models import ConfigModel
+from src.trowel_core.config.parsers import ConfigParserJson
+from src.trowel_core.config.services import ConfigService
+
+@pytest.fixture
+def config_service():
+    return ConfigService(ConfigParserJson())
+
+class TestConfigService:
+    def test_getConfig_should_return_a_model(
+        self, config_service: ConfigService, tmp_path: pathlib.PosixPath
+    ):
+        test_data = { "name": "test", "sources": ["main.c"] }
+        test_file = tmp_path / "trowel.json"
+        test_file.write_text(json.dumps(test_data))
+
+        test_model = ConfigModel(name="test", sources=["main.c"])
+        assert config_service.getConfig(test_file).__dict__ == test_model.__dict__
