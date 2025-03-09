b7efc60ab2fc960e548072404ef9c472638c2e32
Letícia Aguiar <letsaguiar@yandex.com>
2025-03-09 00:51:53 -0300

    feat: setup template service logger

--- src/trowel_core/template/services/template_service.py
+++ src/trowel_core/template/services/template_service.py
@@ -1,9 +1,16 @@
 from trowel_core.config.models import ConfigModel
 from trowel_core.template.builders import TemplateBuilder
+from trowel_core.logger import Logger
 
 class TemplateService:
+    _logger = Logger().getLogger()
+
     def __init__(self, builder: TemplateBuilder):
         self._builder = builder
 
     def build(self, config: ConfigModel) -> str:
-        return self._builder.build(config)
+        self._logger.info("Creating builder template")
+        template = self._builder.build(config)
+        self._logger.info("Seccessfully created builder template")
+
+        return template
