405b945b64dbdc9a9c9fd914ea2ff9610f1a8904
Letícia Aguiar <letsaguiar@yandex.com>
2025-03-09 00:26:38 -0300

    feat: create logger

--- src/trowel_core/setup.py
+++ src/trowel_core/setup.py
@@ -1,4 +1,7 @@
+import logging
 import importlib.resources
+import os
 
+TROWEL_DEBUG_LEVEL = os.getenv("TROWEL_DEBUG_LEVEL", logging.INFO)
 TROWEL_SRC_DIR = importlib.resources.files("trowel_core")
 TROWEL_TEMPLATES_DIR = TROWEL_SRC_DIR.joinpath("resources/templates")
